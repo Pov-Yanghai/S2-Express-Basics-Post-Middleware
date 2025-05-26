a31785b563a7166067c3654445f1bd0ec4cd4724
Pov-Yanghai <Yanghai.pov@student.cadt.edu.kh>
2025-05-26 17:04:35 +0700

    second-commit

--- S2-ExpressBasics+POST+Middleware/Middleware/logger.js
+++ S2-ExpressBasics+POST+Middleware/Middleware/logger.js
@@ -1,5 +1,6 @@
 // middleware/logger.js
 export function logger(req, res, next) {
+ 
   const timestamp = new Date().toISOString();
   console.log(`[${timestamp}] ${req.method} ${req.path} Query:`, req.query);
   next();
